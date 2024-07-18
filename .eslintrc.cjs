52a0a5bc4304968fa355c6f8bf11034bd02e663c
ekdldksp123 <ekdldksp123@naver.com>
2024-07-18 17:00:16 +0900

    :tada: project setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@stylistic/recommended-extends',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/comma-dangle': ['error', 'only-multiline']
+  },
+}
