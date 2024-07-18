803cf746cba8ba986f6635e1cc1fd71fc79320b0
ekdldksp123 <ekdldksp123@naver.com>
2024-07-18 18:15:19 +0900

    :hammer: add tanstack routers

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-export default createClient('https://qeodrbslchmqrfenbgyi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlb2RyYnNsY2htcXJmZW5iZ3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2ODI0ODcsImV4cCI6MjAzNjI1ODQ4N30.LVVR8kkUNHAS_qD1tZWok-cOeG00wtcIAHWm5nLIB30');
+export default createClient('https://qeodrbslchmqrfenbgyi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlb2RyYnNsY2htcXJmZW5iZ3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2ODI0ODcsImV4cCI6MjAzNjI1ODQ4N30.LVVR8kkUNHAS_qD1tZWok-cOeG00wtcIAHWm5nLIB30')
---
+++ src/routes/reports/$reportsId/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import supabaseClient from '@/config/supabase'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/reports/$reportsId/')({
+  loader: async ({ params }) => {
+    const reportId = Number(params.reportId)
+    const [reportResult, groupsResult, accountResult, accountGroupsResult] = await Promise.all([
+      supabaseClient.from('report').select().eq('id', reportId),
+      supabaseClient.from('report_accounts').select('*, report_account_groups!inner(*)').eq('report_id', reportId),
+      supabaseClient.from('report_groups').select().eq('report_id', reportId).order('id', { ascending: true }),
+      supabaseClient.from('report_account_groups').select('*, report_accounts!inner(*)').eq('report_accounts.report_id', reportId),
+    ])
+  },
+  component: () => <div>Hello /reports/$reportsId/!</div>
+})
