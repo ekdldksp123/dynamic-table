8d15db4eda980959353cbc08b2cb407062313688
ekdldksp123 <ekdldksp123@naver.com>
2024-07-25 14:16:44 +0900

    :wrench: add jest config files

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+// /** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^.+\\.svg$': 'jest-svg-transformer',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
