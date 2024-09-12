403f850c5dbde54cadcad195e9a6a42170a56c20
ekdldksp123 <ekdldksp123@naver.com>
2024-09-12 11:24:38 +0900

    :hammer: start to renewal useCreateTableV2

---
+++ src/types/create-table.v2.ts
@@ -0,0 +1,44 @@
+export type ItemValueType = string | string[] | number | boolean | null | undefined;
+
+export type GroupType = 'column' | 'row' | 'value';
+
+export interface ILineItem {
+  id?: number;
+  [key: string]: ItemValueType;
+  base: string;
+}
+
+export interface ILineItemGroup {
+  id: string;
+  name: string;
+  level?: number;
+  type?: GroupType;
+  showTotal: boolean;
+  index: number;
+}
+
+export type LineItemKey = keyof ILineItem;
+
+export type KeyTypeFromItemValue = Exclude<ItemValueType, string[] | boolean | null | undefined | number>;
+
+export interface IReportConfig {
+  id: string;
+  name: string;
+  items: ILineItem[];
+  groups: ILineItemGroup[];
+  rowGroup: ILineItemGroup[];
+  colGroup: ILineItemGroup[];
+  valueGroup: ILineItemGroup[];
+  showRowsTotal: boolean;
+  showColsTotal: boolean;
+}
+
+export type GridGroup = {
+  title: string;
+  key: string;
+  index?: number;
+  children?: GridGroup[];
+  items?: ILineItem[];
+};
+
+// export in
