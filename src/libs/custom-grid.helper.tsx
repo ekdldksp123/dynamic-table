225d30368107579095d96711cb1841f3adfe2f99
ekdldksp123 <ekdldksp123@naver.com>
2024-09-25 16:57:45 +0900

    :sparkles: update getOnlyRowGroupGridData V2 and custom-grid.helper functions

--- src/libs/custom-grid.helper.tsx
+++ src/libs/custom-grid.helper.tsx
@@ -1,6 +1,12 @@
-import { GridColumn } from '@/types';
-import { GridData, ILineItem, ILineItemGroup } from '@/types/create-table.v2';
-import { group } from 'console';
+import { GridColumn, GroupedData } from '@/types';
+import {
+  GridData,
+  GridGroup,
+  ILineItem,
+  ILineItemGroup,
+  KeyTypeFromItemValue,
+  LineItemKey,
+} from '@/types/create-table.v2';
 
 export const getColSpan = (column: GridColumn): number => {
   if (!column.children) return 1;
@@ -83,4 +89,266 @@ export const getDataCountedInGivenUnits = (data: GridData[], unit: number) => {
     }
     return row;
   });
+};
+
+export const getFirstColumn = (group: ILineItemGroup): GridGroup => {
+  const colName = group.name.startsWith('그룹') ? '구분' : group.name;
+  return { key: 'division', title: colName };
+};
+
+export const groupByHierarchical = (data: ILineItem[], keys: LineItemKey[]): GroupedData => {
+  const groupByRecursively = (items: ILineItem[], remainingKeys: LineItemKey[]): GroupedData | ILineItem[] => {
+    if (remainingKeys.length === 0) {
+      return items;
+    }
+    const [currentKey] = remainingKeys;
+    return items.reduce((result, item) => {
+      const groupKey = item[currentKey] as unknown as KeyTypeFromItemValue;
+      if (!result[groupKey]) {
+        result[groupKey] = [];
+      }
+      (result[groupKey] as unknown as ILineItem[]).push(item);
+      return result;
+    }, {} as GroupedData);
+  };
+
+  const nestedGroupBy = (groupedData: GroupedData, keys: LineItemKey[]): GroupedData => {
+    if (keys.length === 0) return groupedData;
+
+    const [currentKey, ...nextKeys] = keys;
+    for (const key in groupedData) {
+      if (Array.isArray(groupedData[key])) {
+        groupedData[key] = groupByRecursively(groupedData[key] as unknown as ILineItem[], [currentKey]);
+        nestedGroupBy(groupedData[key] as GroupedData, nextKeys);
+      }
+    }
+    return groupedData;
+  };
+
+  const initialGroup = groupByRecursively(data, [keys[0]]);
+  return nestedGroupBy(initialGroup as GroupedData, keys.slice(1));
+};
+
+export const getGroupValuesAndCodes = (items: ILineItem[], groupId: string) => {
+  const groupValues: string[] = [];
+  const lineItemsMap: Record<string, ILineItem[]> = {};
+
+  for (const item of items) {
+    const itemValue = item[groupId] as unknown as KeyTypeFromItemValue;
+
+    if (!groupValues.includes(itemValue)) {
+      groupValues.push(itemValue);
+      lineItemsMap[itemValue] = [item];
+    } else {
+      lineItemsMap[itemValue] = [...lineItemsMap[itemValue], item];
+    }
+  }
+
+  return { groupValues, lineItemsMap };
+};
+
+interface ITransformToGridGroup {
+  groupedData: GroupedData;
+  groups: ILineItemGroup[];
+  showTotal: boolean;
+  minGroupValues: string[];
+  lineItemsMap: Record<string, ILineItem[]>;
+  axis?: 'col' | 'row';
+  values?: string[];
+}
+
+export const transformToGridGroup = ({
+  groupedData,
+  groups,
+  showTotal,
+  minGroupValues,
+  lineItemsMap,
+  axis,
+  values,
+}: ITransformToGridGroup): {
+  gridGroups: GridGroup[];
+  columnsKeyValueMap: Record<string, ILineItem[]>;
+} => {
+  const gridGroups: GridGroup[] = [];
+  const columnsKeyValueMap: Record<string, ILineItem[]> = {};
+
+  const lengthOfGroup = groups.length;
+  const minGroupName = groups[lengthOfGroup - 1].name;
+  const showSubtotalGroups = groups.filter(({ showTotal }) => showTotal === true);
+
+  const hasDuplicateKeys = checkSubgroupKeys(groupedData);
+  const items: ILineItem[] = [];
+
+  const traverse = (data: GroupedData | ILineItem[], parentName: string | null, index = 0): GridGroup[] => {
+    if (Array.isArray(data)) {
+      items.push(...data);
+      if (axis === 'col' && values?.length) {
+        for (const valueKey of values) {
+          columnsKeyValueMap[`${parentName}_${valueKey}`] = data;
+        }
+      }
+      return [];
+    }
+
+    return Object.keys(data)
+      .filter((key) => key !== 'groupId')
+      .map((key) => {
+        const currentName = parentName ? `${parentName}_${key}` : key;
+
+        const children = traverse(data[key] as GroupedData | ILineItem[], currentName, index + 1);
+
+        const groupDef: GridGroup = {
+          title: key,
+          key: currentName,
+          index,
+          items: Array.isArray(data[key]) ? (data[key] as unknown as ILineItem[]) : undefined,
+        };
+
+        if (children.length) {
+          //한글 순으로 정렬
+          children.sort((a, b) => (a.title < b.title ? -1 : a.title > b.title ? 1 : 0));
+
+          if (showSubtotalGroups.find(({ index: _idx }) => _idx === index + 1) && children.length > 1) {
+            const subtotalGroup: GridGroup = {
+              index: index + 1,
+              key: `${currentName}_subtotal`,
+              title: lengthOfGroup >= 2 && index < -lengthOfGroup - 2 ? '소계' : '합계',
+              items: children.flatMap((child) => child.items ?? []),
+            };
+
+            children.push(subtotalGroup);
+          }
+
+          if (lengthOfGroup > 2 && hasDuplicateKeys && index === 0) {
+            const subtotalGroup: GridGroup = {
+              index: index + 1,
+              key: `${currentName}_subtotal`,
+              title: '합계',
+              children: minGroupValues.map(
+                (value) =>
+                  ({
+                    key: `${currentName}_${value}_subtotal`,
+                    title: value,
+                    items: lineItemsMap[value],
+                  }) as GridGroup,
+              ),
+            };
+            children.push(subtotalGroup);
+          }
+          groupDef.children = children as GridGroup[];
+        }
+        return groupDef;
+      });
+  };
+
+  gridGroups.push(...traverse(groupedData, null));
+
+  const showSubtotalGroupsWithoutTotal = showSubtotalGroups.filter(({ index }) => index !== 0);
+
+  if (lengthOfGroup === 2 && hasDuplicateKeys && showSubtotalGroupsWithoutTotal.length) {
+    const subtotalGroup: GridGroup = {
+      index: 0,
+      key: `${minGroupName}_subtotals`,
+      title: '합계',
+      children: minGroupValues.map((value) => ({
+        key: `${minGroupName}_${value}_subtotal`,
+        title: value,
+        items: lineItemsMap[value],
+      })),
+    };
+
+    for (const value of minGroupValues) {
+      columnsKeyValueMap[`${minGroupName}_${value}_subtotal`] = lineItemsMap[value];
+    }
+
+    gridGroups.push(subtotalGroup);
+  }
+
+  if (showSubtotalGroups.find(({ index }) => index === 0) || showTotal) {
+    gridGroups.push({ key: `${axis}_total`, title: '총계', items });
+  }
+
+  return { gridGroups, columnsKeyValueMap };
+};
+
+export const checkSubgroupKeys = (groupedData: GroupedData): boolean => {
+  const keySet: Set<string> = new Set();
+
+  const traverse = (data: GroupedData | ILineItem[]): boolean => {
+    if (Array.isArray(data)) {
+      return false;
+    }
+    let hasDuplicateKeys = false;
+
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        if (keySet.has(key)) {
+          hasDuplicateKeys = true;
+          break;
+        }
+        keySet.add(key);
+
+        const value = data[key];
+        //배열이 아닌 경우에만 재귀적으로 탐색
+        if (typeof value === 'object' && !Array.isArray(value)) {
+          hasDuplicateKeys = traverse(value);
+        }
+
+        if (hasDuplicateKeys) {
+          break;
+        }
+      }
+    }
+    return hasDuplicateKeys;
+  };
+
+  return traverse(groupedData);
+};
+
+interface IGetGroupedData {
+  rows: GridGroup[];
+  columns: Record<string, ILineItem[]>;
+  values: string[];
+}
+
+export const getGroupedData = ({ rows, columns, values }: IGetGroupedData) => {
+  const data: GridData[] = [];
+  const columnKeys = Object.keys(columns);
+
+  const recur = ({ items, key, children }: GridGroup) => {
+    if (!items && children) {
+      for (const child of children) {
+        recur(child);
+      }
+    } else {
+      const row: GridData = { division: key };
+
+      if (columnKeys.length) {
+        for (const colKey of columnKeys) {
+          const colItems = columns[colKey];
+          for (const valueKey of values) {
+            row[colKey] = items
+              ?.filter(({ id }) => colItems.find((item) => id === item.id))
+              .reduce((sum, cur) => {
+                const value = Number(cur[valueKey]);
+                return sum + (Number.isNaN(value) ? 0 : value);
+              }, 0);
+          }
+        }
+      } else {
+        for (const valueKey of values) {
+          row[valueKey] = items?.reduce((sum, cur) => {
+            const value = Number(cur[valueKey]);
+            return sum + (Number.isNaN(value) ? 0 : value);
+          }, 0);
+        }
+      }
+      data.push(row);
+    }
+  };
+
+  for (const group of rows) {
+    recur(group);
+  }
+  return data;
 };
--- src/libs/hooks/useCreateTableV2.tsx
+++ src/libs/hooks/useCreateTableV2.tsx
@@ -1,5 +1,13 @@
 import { GridData, GridGroup, ILineItem, ILineItemGroup } from '@/types/create-table.v2';
-import { getDataCountedInGivenUnits } from '../custom-grid.helper';
+import {
+  getDataCountedInGivenUnits,
+  getFirstColumn,
+  getGroupValuesAndCodes,
+  getGroupedData,
+  groupByHierarchical,
+  transformToGridGroup,
+} from '../custom-grid.helper';
+import { GroupedData } from '@/types';
 
 interface IGetGridData {
   lineItems: ILineItem[];
@@ -9,6 +17,12 @@ interface IGetGridData {
 interface IGetBasicGridData extends IGetGridData {
   groupHeaders: ILineItemGroup[];
   fieldHeaders: string[];
+}
+
+interface IGetOnlyRowGroupGridData extends IGetBasicGridData {
+  rowGroup: ILineItemGroup[];
+  valueGroup: ILineItemGroup[];
+  showRowsTotal: boolean;
 }
 
 export const useCreateTableV2 = () => {
@@ -49,4 +63,50 @@ export const useCreateTableV2 = () => {
 
     return { columns, rows: [], data: getDataCountedInGivenUnits(data, amountUnit) };
   };
+
+  const getOnlyRowGroupGridData = ({
+    lineItems,
+    rowGroup,
+    valueGroup,
+    showRowsTotal,
+    amountUnit,
+  }: IGetOnlyRowGroupGridData) => {
+    rowGroup = rowGroup.map((g, i) => {
+      g.index = i;
+      return g;
+    });
+
+    const columns: GridGroup[] = [getFirstColumn(rowGroup[0])];
+
+    for (const { id } of valueGroup) {
+      columns.push({ key: id, title: id });
+    }
+
+    const groupedRowData: GroupedData = groupByHierarchical(
+      lineItems,
+      rowGroup.map(({ id }) => id),
+    );
+
+    const minGroup = rowGroup[rowGroup.length - 1];
+    const { groupValues, lineItemsMap } = getGroupValuesAndCodes(lineItems, minGroup.id);
+
+    const { gridGroups: rows } = transformToGridGroup({
+      groupedData: groupedRowData,
+      groups: rowGroup,
+      showTotal: showRowsTotal,
+      minGroupValues: groupValues,
+      lineItemsMap,
+    });
+
+    const values = valueGroup.map(({ id }) => id);
+    const data = getGroupedData({ rows, columns: {}, values });
+
+    return {
+      columns,
+      rows,
+      data: getDataCountedInGivenUnits(data, amountUnit),
+    };
+  };
+
+  return { getBasicGridData, getOnlyRowGroupGridData };
 };
